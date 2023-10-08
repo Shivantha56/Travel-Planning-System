78f29c14ee73be7b83bdfd6fd4111bb0a2ec75c2
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-10-09 01:18:29 +0530

    responsive navigation

---
+++ frontend/assets/script/home/home.js
@@ -0,0 +1,26 @@
+let sideMenu = document.querySelector(".side-menu");
+let menuBtnClose = document.querySelector(".menu-btn-close");
+let menuBtnOpen = document.querySelector(".menu-btn");
+
+document.querySelector(".menu-btn").onclick = function () {
+    sideMenu.style.left = "0"
+    sideMenu.style.transition = "0.5s";
+    menuBtnClose.style.display = "inline-block";
+    menuBtnOpen.style.display = "none";
+}
+document.querySelector(".menu-btn-close").onclick = function () {
+    sideMenu.style.left = "-250px"
+    sideMenu.style.transition = "0.5s";
+    menuBtnClose.style.display = "none";
+    menuBtnOpen.style.display = "inline-block";
+}
+
+addEventListener("resize", (event) => {
+    if (window.innerWidth >= 1100){
+        menuBtnClose.style.display = "none";
+        menuBtnOpen.style.display = "none";
+        sideMenu.style.left = "-250px"
+    }else{
+        menuBtnOpen.style.display = "inline-block";
+    }
+})

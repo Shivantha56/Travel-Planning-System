a78c5a9bc7e555cda804e5b44ce6bc2885946703
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-10-15 05:58:59 +0530

    add guide controller class to the project

---
+++ backend/GuideService/src/main/java/lk/nextTravel/guideService/GuideService/controller/GuideController.java
@@ -0,0 +1,14 @@
+package lk.nextTravel.guideService.GuideService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/guide")
+public class GuideController {
+
+
+
+
+
+}

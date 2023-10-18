020c9d33b14f5e27d656a736163ea697945ce498
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-10-18 23:19:20 +0530

    added new bean ModelMapper

--- backend/HotelService/src/main/java/lk/nextTravel/hotelService/HotelService/HotelServiceApplication.java
+++ backend/HotelService/src/main/java/lk/nextTravel/hotelService/HotelService/HotelServiceApplication.java
@@ -1,11 +1,18 @@
 package lk.nextTravel.hotelService.HotelService;
 
+import lk.nextTravel.hotelService.HotelService.util.ModelsMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HotelServiceApplication {
 
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServiceApplication.class, args);
 	}

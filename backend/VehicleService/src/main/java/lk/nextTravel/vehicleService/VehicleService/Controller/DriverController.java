7b67cbb64a984e0c2f1778568cf3e6f867ed8513
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-10-20 14:23:20 +0530

    package structure

---
+++ backend/VehicleService/src/main/java/lk/nextTravel/vehicleService/VehicleService/Controller/DriverController.java
@@ -0,0 +1,10 @@
+package lk.nextTravel.vehicleService.VehicleService.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class DriverController {
+}
---
+++ backend/VehicleService/src/main/java/lk/nextTravel/vehicleService/VehicleService/Controller/VehicelController.java
@@ -0,0 +1,9 @@
+package lk.nextTravel.vehicleService.VehicleService.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class VehicelController {
+}
